f6408b08fdf1d51d5d8f5458fda835983ba13401
jesus1496 <shuan346@gmail.com>
2025-06-23 18:11:18 +0200

    update import

---
+++ siw-game/src/main/resources/import.sql
@@ -0,0 +1,11 @@
+INSERT INTO console (id, anno, name, url_image) VALUES (1, 1989, 'Super Nintendo', '/images/console/super.jpg');
+INSERT INTO console (id, anno, name, url_image) VALUES (2, 1994, 'PlayStation', '/images/console/play.jpg'); 
+INSERT INTO console (id, anno, name, url_image) VALUES (3, 1994, 'Sega Saturn', '/images/console/sega.jpg'); 
+INSERT INTO console (id, anno, name, url_image) VALUES (4, 2000, 'Xbox', '/images/console/xbox.jpg'); 
+INSERT INTO console (id, anno, name, url_image) VALUES (5, 1989, 'PlayStation Portable', '/images/console/Psp.jpg');
+
+INSERT INTO games (id, name, url_of_picture) VALUES (1, 'Super Metroid', '/images/games/th.jpg');
+INSERT INTO games (id, name, url_of_picture) VALUES (2, 'Metal Gear Solid', '/images/games/metal.jpg');
+INSERT INTO games (id, name, url_of_picture) VALUES (3, 'Megaman X4', '/images/games/mega.jpg');
+INSERT INTO games (id, name, url_of_picture) VALUES (4, 'Halo', '/images/games/halo.jpg');
+INSERT INTO games (id, name, url_of_picture) VALUES (5, 'Crash', '/images/games/crash.jpg');
